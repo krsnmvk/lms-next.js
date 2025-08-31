86559b1adab8fca290a1b3596d220bab07e6db9e
krsnmvk <krsnmvk@gmail.com>
2025-08-31 15:42:39 +0700

    03: setup authentication and database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { authClient } from '@/lib/auth-client';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 export default function Home() {
+  const { data: session } = authClient.useSession();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) return router.push('/sign-in');
+  }, [session, router]);
+
   return (
     <div>
       <h1>Home Page</h1>
+
+      <Button onClick={async () => await authClient.signOut()}>Logout</Button>
     </div>
   );
 }
